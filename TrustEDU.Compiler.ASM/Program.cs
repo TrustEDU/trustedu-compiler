7bae37429f24ef9c89630e63d9bec27c0fe666b3
MacUser <macuser@SERIVN-MAC.local>
2018-09-29 23:16:05 +0700

    First init the TrustEDU.Compiler projects

---
+++ TrustEDU.Compiler.ASM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrustEDU.Compiler.ASM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TrustEDU.Compiler.Dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrustEDU.Compiler.Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
